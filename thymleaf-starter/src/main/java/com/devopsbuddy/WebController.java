50509777425317e2173d24a2b1ad8e0bd1e497c0
gaurav1116 <gaurav.1116@outlook.com>
2021-10-06 14:44:52 +0530

    Initial commit

---
+++ thymleaf-starter/src/main/java/com/devopsbuddy/WebController.java
@@ -0,0 +1,16 @@
+package com.devopsbuddy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+	
+	@GetMapping("/welcome")
+	public String Welcome(Model model) {
+		
+		model.addAttribute("message"," Welcome to our Website" );
+		return("index");
+	}
+}
---
+++ thymleaf-starter/src/test/java/com/devopsbuddy/ThymleafStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
